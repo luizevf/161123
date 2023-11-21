665574e9dd3b66a398e9fbe0de81845ee85489ea
Luiz Eduardo Vieira de Freitas <62312229+luizevf@users.noreply.github.com>
2023-11-21 16:54:34 -0300

    Add files via upload

---
+++ controllers/ControllerUsuario.java
@@ -0,0 +1,15 @@
+import org.springframework.web.bind.annotation.RestController;
+import com.SA.bibliocrud.services.ServiceUsuario;
+
+@RestController
+public class ControllerUsuario{
+
+
+private ServiceUsuario = serviceUsuario;
+
+public ControllerUsuario (ServiceUsuario serviceUsuario){
+
+this.ServiceUsuario = ServiceUsuario;
+}
+
+ }
